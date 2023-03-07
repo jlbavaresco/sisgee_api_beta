016ec48491b0f0852144d3bcf54163f1a71b9668
jlbavaresco@gmail.com <jlbavaresco@gmail.com>
2023-03-07 11:41:03 -0300

    Ajustando o config para não usar o dotenv

--- config.js
+++ config.js
@@ -1,17 +1,23 @@
-require('dotenv').config()
+const { Pool } = require('pg')
 
 const isProduction = process.env.NODE_ENV === 'production'
 
-const {Pool} = require('pg')
+let pool = null;
+if (isProduction) {
+  pool = new Pool({
+    connectionString: process.env.DATABASE_URL, ssl: {
+      rejectUnauthorized: false,
+    }
+  })
+} else {
+  pool = new Pool({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'SISGEE',
+    password: 'postgres',
+    port: 5432
+  })
+}
 
-const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`
 
-const pool = new Pool({
-  connectionString: isProduction ? process.env.DATABASE_URL: connectionString
-  // descomentar para fazer deploy no heroku
-  //, ssl: {
-  //  rejectUnauthorized: false,
-  //}
-})
-
-module.exports = {pool}
+module.exports = { pool }
